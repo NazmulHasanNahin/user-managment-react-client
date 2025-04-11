4dcae0c847ae3918c3afde246522c85405dbb43c
NazmulHasanNahin <nahinn75@gmail.com>
2025-04-11 20:36:05 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-base-100">
+      <div className="p-6 shadow-xl bg-white rounded-xl text-center">
+        <h1 className="text-3xl font-bold mb-4 text-primary">Welcome</h1>
+        <button className="btn btn-primary">Click Me</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/UpdateUser.jsx
@@ -0,0 +1,9 @@
+const UpdateUser = () => {
+    return (
+        <div>
+           <h1>update user jsx</h1>
+        </div>
+    );
+};
+
+export default UpdateUser;
